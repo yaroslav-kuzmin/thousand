00d8b0af797ff7f10cec92f90b7573c455d7ca5a
Kuzmin Yaroslav <kuzmin.yaroslav@gmail.com>
2013-10-10 18:02:17 +0600

    Преобразовал скрипт для создания конфигурационных файлов добавил создание файла access

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+################################ VARIABLE #####################################
+HEADER='
 ###############################################################################
 #                                                                             #
 # Thousand - card game                                                        #
@@ -20,7 +23,17 @@
 #   Email contact: esdt@mail.ru                                               #
 #                                                                             #
 ###############################################################################
+'
 DIR_ROOT=.thousand
+FILE_INI=ini
+DIR_LOG=log
+DIR_DATABASE=database
+FILE_ACCESS=access
+
+################################ FUNCTION #####################################
+
+################################## MAIN ######################################
+
 cd $HOME
 mkdir $DIR_ROOT 2>&- 
 CHECK=$?
@@ -30,48 +43,29 @@ then
 	exit 0;
 fi
 printf "Создан рабочий каталог\n"
-###############################################################################
+
+##############
+
 cd $DIR_ROOT
-exec 10> ini
+exec 10> ${FILE_INI}
 cat >&10 << _ANYEOF
-###############################################################################
-#                                                                             #
-# Thousand - card game                                                        #
-#  Copyright (C) <2013> <Kuzmin Yaroslav>                                     #
-#                                                                             #
-#  This program is free software: you can redistribute it and/or modify       #
-#  it under the terms of the GNU General Public License as published by       #
-#  the Free Software Foundation, either version 3 of the License, or          #
-#  (at your option) any later version.                                        #
-#                                                                             #
-#  This program is distributed in the hope that it will be useful,            #
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of             #
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
-#  GNU General Public License for more details.                               #
-#                                                                             #
-#  You should have received a copy of the GNU General Public License          #
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.      #
-#                                                                             #
-#   Email contact: esdt@mail.ru                                               #
-#                                                                             #
-###############################################################################
-
-[global]
+${HEADER}
 # Глобальные настройки
+[global]
 
-[server]
 #Настройки сервера
+[server]
 
-[client]
 #Настройки клиента
+[client]
 
-[robot]
 #Настройки робота
+[robot]
 
 _ANYEOF
 printf "Создан файл ini\n"
-###############################################################################
-DIR_LOG=log
+
+##############
 
 mkdir $DIR_LOG 2>&- 
 CHECK=$?
@@ -81,8 +75,8 @@ then
 	exit 0;
 fi
 printf "Создан каталог %s\n" $DIR_LOG
-###############################################################################
-DIR_DATABASE=database
+
+##############
 
 mkdir $DIR_DATABASE 2>&- 
 CHECK=$?
@@ -92,6 +86,22 @@ then
 	exit 0;
 fi
 printf "Создан каталог %s\n" $DIR_DATABASE
-###############################################################################
+
+cd ${DIR_DATABASE}
+exec 11> ${FILE_ACCESS} 
+cat >&11 << _ANYEOF
+${HEADER}
+#Доступ для роботов
+[robot]
+
+#доступ для игроков
+[user]
+admin=00000000000000000000000000000000
+
+_ANYEOF
+printf "Создан файл доступа %s\n" ${FILE_ACCESS}
+
+cd ..
+
 exit 0
 ###############################################################################
