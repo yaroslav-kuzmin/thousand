2d9bf9a8d9a35f528b17565f7563fcaa11730f95
Kuzmin Yaroslav <kuzmin.yaroslav@gmail.com>
2013-10-10 17:43:19 +0600

    Добавил модуль отправки команд и сообщений игрокам от сервера

---
+++ server/command.c
@@ -0,0 +1,57 @@
+/*****************************************************************************/
+/*                                                                           */
+/* Thousand - card game                                                      */
+/*  Copyright (C) <2013> <Kuzmin Yaroslav>                                   */
+/*                                                                           */
+/*  This program is free software: you can redistribute it and/or modify     */
+/*  it under the terms of the GNU General Public License as published by     */
+/*  the Free Software Foundation, either version 3 of the License, or        */
+/*  (at your option) any later version.                                      */
+/*                                                                           */
+/*  This program is distributed in the hope that it will be useful,          */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of           */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            */
+/*  GNU General Public License for more details.                             */
+/*                                                                           */
+/*  You should have received a copy of the GNU General Public License        */
+/*  along with this program.  If not, see <http://www.gnu.org/licenses/>.    */
+/*                                                                           */
+/*   Email contact: esdt@mail.ru                                             */
+/*                                                                           */
+/*****************************************************************************/
+
+/*****************************************************************************/
+/* Модуль посылает команды игрокам                                           */
+/*****************************************************************************/
+
+/*****************************************************************************/
+/* Дополнительные файлы                                                      */
+/*****************************************************************************/
+
+/*****************************************************************************/
+/* Глобальные переменые                                                      */
+/*****************************************************************************/
+
+/*****************************************************************************/
+/* Вспомогательные функция                                                   */
+/*****************************************************************************/
+
+/*****************************************************************************/
+/* Основная функция                                                          */
+/*****************************************************************************/
+
+int cmd_check_connect(user_s * us)
+{
+	int rc;
+	int fd = us->fd;
+	message_cmd_u temp;
+
+	temp.field.type = CMD_CHECK_CONNECT;
+	temp.field.len = 0;
+	us->package++;
+	temp.field.number = us->package;
+	rc = send(fd,temp.array,(sizeof(message_cmd_s)),0);
+
+	return rc;
+}
+/*****************************************************************************/
